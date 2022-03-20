935bc363695a8200dd130f5c19daa350467af623
YRMING <yrm@outlook.com>
2022-03-21 00:10:27 +0800

    feat: implement getTreeItem and getChildren methods

--- src/tree/questions/QuestionsTreeProvider.ts
+++ src/tree/questions/QuestionsTreeProvider.ts
@@ -1,17 +1,23 @@
 import { Event, EventEmitter, ProviderResult, TreeDataProvider } from 'vscode';
 import { QuestionTreeItem } from './QuestionTreeItem';
+import { getQuestions } from '../../data';
+import { Question } from '../../type';
 
 export class QuestionsTreeProvider implements TreeDataProvider<QuestionTreeItem> {
     private _onDidChangeTreeData: EventEmitter<QuestionTreeItem | undefined | void> = new EventEmitter<QuestionTreeItem | undefined | void>();
     readonly onDidChangeTreeData: Event<QuestionTreeItem | undefined | void> = this._onDidChangeTreeData.event;
 
     getTreeItem(element: QuestionTreeItem): QuestionTreeItem | Thenable<QuestionTreeItem> {
-        // throw new Error('Method not implemented.');
         return element;
     }
     getChildren(element?: QuestionTreeItem): ProviderResult<QuestionTreeItem[]> {
-        // throw new Error('Method not implemented.');
-        return Promise.resolve([]);
+        const questions: Question[] = getQuestions();
+        const treeItems: QuestionTreeItem[] = [];
+        questions.forEach(question => {
+            const treeItem = new QuestionTreeItem(`${question.idx!} - ${question.title!}`);
+            treeItems.push(treeItem);
+        });
+        return treeItems;
     }
 
 }
