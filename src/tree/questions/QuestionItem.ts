5901b8882d12beac330ba9ac83a5a464d74bb9cd
YRMING <yrm@outlook.com>
2022-06-11 20:56:41 +0800

    chore: adjust parameter order

--- src/tree/questions/QuestionItem.ts
+++ src/tree/questions/QuestionItem.ts
@@ -4,9 +4,9 @@ export class QuestionItem extends TreeItem {
   constructor(
     public readonly label: string,
     public readonly command?: Command,
+    public readonly iconPath?: string | ThemeIcon,
     public readonly tooltip?: string,
-    public readonly collapsibleState?: TreeItemCollapsibleState,
-    public readonly iconPath?: string | ThemeIcon
+    public readonly collapsibleState?: TreeItemCollapsibleState
   ) {
     super(label)
   }
